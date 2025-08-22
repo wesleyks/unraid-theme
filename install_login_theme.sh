610806411b54a1b00c1b1172ce01866debca0748
wesley sun <sunwes@gmail.com>
2025-08-21 22:17:30 -0700

    add cache busting to install script

--- install_login_theme.sh
+++ install_login_theme.sh
@@ -34,6 +34,6 @@ fi
 # Adding stylesheets
 if ! grep -q "custom-theme.css" ${LOGIN_PAGE}; then
   echo "Adding custom stylesheet"
-  sed -i -e "\@<style>@i\    <link data-tp='custom' rel='stylesheet' href='https://cdn.jsdelivr.net/gh/wesleyks/unraid-theme@main/custom-theme.min.css'>" ${LOGIN_PAGE}
+  sed -i -e "\@<style>@i\    <link data-tp='custom' rel='stylesheet' href='https://cdn.jsdelivr.net/gh/wesleyks/unraid-theme@main/custom-theme.min.css?v=$(date +%s)'>" ${LOGIN_PAGE}
   echo 'Custom stylesheet added'
 fi
